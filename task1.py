5c0bfde763144776c49a92a6ca996cf63ed4c8b7
andriiklykavkaa <andriiklykavkaa@gmail.com>
2024-11-08 16:59:53 +0200

    Completed full task

--- task1.py
+++ task1.py
@@ -1,47 +1,46 @@
-NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "B", "C", "D", "E", "F"]
 
-def convert_to_binary(str_num: str) -> int:
-    if not str_num.isdigit():
-        print("Unable to convert to decimal.")
-        return None
+NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 
-    binary_num = int(str_num)
-    result_num = ""
+def convert_to_decimal(str_num: str, depart_system: int) -> int:
 
-    while binary_num > 0:
-        bin_remainder = binary_num % 2
-        result_num = str(bin_remainder) + result_num
-        binary_num = binary_num // 2
-    return result_num
+    for char in str_num:
+        if char not in NUMBERS[:depart_system]:
+            print("The number does not belong to the system. Please try again")
+            return None
 
-def convert_to_decimal(str_num: str) -> int:
-    if not str_num.startswith("0b") and str_num[2:].isdigit():
-        print("Unable to convert to binary. Please enter valid binary number.")
-        return None
-
-
-    decimal_num = str_num[2:][::-1]
     result_num = 0
 
-    for i, char in enumerate(decimal_num):
-        result_num += int(char) * pow(2, i)
+
+    for i, char in enumerate(str_num[::-1]):
+        result_num += int(NUMBERS.index(char)) * depart_system ** i
+
     return result_num
 
+def convert_from_decimal(num: int, arrive_system) -> str:
+
+    decimal_num = num
+    result_num = ""
+
+    while decimal_num > 0:
+        remainder = decimal_num % arrive_system
+        result_num = NUMBERS[remainder] + result_num
+        decimal_num = decimal_num // arrive_system
+    return result_num
+
+def convert_number(number: str, depart_system: int, arrive_system: int) -> str:
+    decimal_number = convert_to_decimal(number, depart_system)
+    arrived_number = convert_from_decimal(decimal_number, arrive_system)
+    return arrived_number
 
 def run_converter():
     while True:
-        number = input("Please enter a number with a prefix: ")
-        if number.startswith("0b"):
-            result = convert_to_decimal(number)
-            print("Your number in decimal form is: ", result)
-        else:
-            result = convert_to_binary(number)
-            print("Your number in binary form is: ", result)
-
-        command = input("Would you like to continue? (y/n)")
-        if command == "n":
-            break
-        if command != "y":
-            print("Unrecognized command. Please try again.")
+        number = input("Please enter a number: ")
+        depart_system = int(input("Please enter number system of this number: "))
+        arrive_system = int(input("Please enter system you want to convert to: "))
+        if depart_system > 16 or arrive_system > 16:
+            print('The systems must be in range of 16')
+            continue
+        result = convert_number(number, depart_system, arrive_system)
+        print(result)
 
 run_converter()
