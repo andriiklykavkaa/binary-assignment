4a0f19a689e9a9f8056a5b5ef9006dc696a0a1d8
andriiklykavkaa <andriiklykavkaa@gmail.com>
2024-11-08 17:16:06 +0200

    Added annotation and fixed bugs

--- task1.py
+++ task1.py
@@ -4,14 +4,14 @@ NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D",
 def convert_to_decimal(str_num: str, depart_system: int) -> int:
 
     for char in str_num:
-        if char not in NUMBERS[:depart_system]:
+        if char.upper() not in NUMBERS[:depart_system]:
             print("The number does not belong to the system. Please try again")
             return None
 
     result_num = 0
 
-
     for i, char in enumerate(str_num[::-1]):
+        char = char.upper()
         result_num += int(NUMBERS.index(char)) * depart_system ** i
 
     return result_num
@@ -29,18 +29,36 @@ def convert_from_decimal(num: int, arrive_system) -> str:
 
 def convert_number(number: str, depart_system: int, arrive_system: int) -> str:
     decimal_number = convert_to_decimal(number, depart_system)
-    arrived_number = convert_from_decimal(decimal_number, arrive_system)
-    return arrived_number
+    if decimal_number is not None:
+        arrived_number = convert_from_decimal(decimal_number, arrive_system)
+        return arrived_number
+
 
 def run_converter():
     while True:
         number = input("Please enter a number: ")
-        depart_system = int(input("Please enter number system of this number: "))
-        arrive_system = int(input("Please enter system you want to convert to: "))
-        if depart_system > 16 or arrive_system > 16:
-            print('The systems must be in range of 16')
+        depart_system = input("Please enter number system of this number: ")
+        arrive_system = input("Please enter system you want to convert to: ")
+
+        if depart_system.isdigit() and arrive_system.isdigit():
+            depart_system = int(depart_system)
+            arrive_system = int(arrive_system)
+            if (depart_system > 16 or depart_system < 1) or (arrive_system > 16 or arrive_system < 1):
+                print('The systems must be in range 1 to 16')
+                continue
+        else:
+            print('The systems must be integers')
             continue
+
         result = convert_number(number, depart_system, arrive_system)
         print(result)
+
+        command = input("Would you like to continue? (y/n): ")
+        if command.upper() == 'N':
+            break
+        elif command.upper() == 'Y':
+            pass
+        else:
+            print("Unrecognized command")
 
 run_converter()
